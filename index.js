36e75617464e3f008f8e8bdc765a6738bf787ac0
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2019-11-23 01:12:32 +0100

    Melo Funny Games Done

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-$(document).ready(function(){
+var sentence = "Página en Desarrollo";
+var sentenceArray = sentence.split("");
+var sentenceLength = sentence.length;
 
-$('#pathLogo2').click(function(){
+$(document).ready(function() {
+  $("#pathLogo2").click(function() {
+    $(this).removeClass("animated");
+    $("#logo2").addClass("logoClicked");
+    $("#mainTextContainer").fadeIn(300);
+    setTimeout(typeWriter, 400);
+  });
 
-    $(this).removeClass('animated');
-    $('#logo2').addClass('logoClicked');
+  $(".menuItem").click(function() {
+    $(".divDoc").fadeOut(400);
 
+    var thisId = $(this).attr("id");
+
+    $("#div" + thisId).fadeIn(500);
+    $("#fondoBlanco").fadeIn(500);
+    $("#MenuContainer").fadeIn(500);
+    $("#MenuContainer").css("display", "flex");
+    $(".leftMenu,.rightMenu").css("display", "flex");
+  });
+
+  $(".wordRightMenu").click(function() {
+    $(".divDoc").fadeOut(100);
+    var thisId = $(this).attr("data-id");
+    setTimeout(function() {
+      $("#" + thisId).fadeIn(300);
+    }, 200);
+  });
+
+  $("#x_blanca").click(function() {
+    $("#fondoBlanco").fadeOut(500);
+    $("#MenuContainer").fadeOut(500);
+    $("#MenuContainer").fadeOut(500);
+    $(".leftMenu,.rightMenu").fadeOut(500);
+  });
+
+  $(".cross").click(function() {
+    $("#x_blanca").trigger("click");
+  });
 });
-    
-});
+
+var i = 0;
+var txt = "Página en Desarrollo";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("mainTextContainer").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
